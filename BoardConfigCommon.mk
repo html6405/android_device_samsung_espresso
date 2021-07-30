dfa30f5694a2589dd1b0c65f334c4cedc2337863
MightyM17 <mightymb17@gmail.com>
2021-07-30 23:12:43 +0530

    BoardConfigCommon: Disable building sepolicy for now

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -71,8 +71,8 @@ BOARD_HAVE_BLUETOOTH_BCM := true
 BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/samsung/espresso/bluetooth
 
 # SELinux
-BOARD_SEPOLICY_DIRS += \
-    device/samsung/espresso/sepolicy
+# BOARD_SEPOLICY_DIRS += \
+    # device/samsung/espresso/sepolicy
 
 # Recovery
 RECOVERY_FSTAB_VERSION := 2
